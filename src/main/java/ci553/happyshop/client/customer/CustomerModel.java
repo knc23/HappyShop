8b0570c95177f6cf8bde4c4b68b6b2cf1a925ded
karyC0120 <karyching2006@gmail.com>
2025-10-29 15:54:43 +0000

    add up the amount number of product instead of showing duplicate product line by line

--- src/main/java/ci553/happyshop/client/customer/CustomerModel.java
+++ src/main/java/ci553/happyshop/client/customer/CustomerModel.java
@@ -70,7 +70,8 @@ public class CustomerModel {
             //TODO
             // 1. Merges items with the same product ID (combining their quantities).
             // 2. Sorts the products in the trolley by product ID.
-            trolley.add(theProduct);
+            // trolley.add(theProduct);
+            makeOrganizedTrolley();
             displayTaTrolley = ProductListFormatter.buildString(trolley); //build a String for trolley so that we can show it
         }
         else{
@@ -79,6 +80,17 @@ public class CustomerModel {
         }
         displayTaReceipt=""; // Clear receipt to switch back to trolleyPage (receipt shows only when not empty)
         updateView();
+    }
+
+    void makeOrganizedTrolley(){
+        for ( Product product : trolley) {
+            if ( product.getProductId().equals(theProduct.getProductId())) {
+                product.setOrderedQuantity(product.getOrderedQuantity() + theProduct.getOrderedQuantity());
+                return;
+            }
+        }
+        Product newProduct = new Product(theProduct.getProductId(), theProduct.getProductDescription(), theProduct.getProductImageName(), theProduct.getUnitPrice(), theProduct.getStockQuantity());
+        trolley.add(newProduct);
     }
 
     void checkOut() throws IOException, SQLException {
